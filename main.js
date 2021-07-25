44197902645e31841794866ad96855e7e075a178
davidatb <davidabrahamtellezbautista@hotmail.com>
2021-07-24 22:23:52 -0500

    first commit from slider

---
+++ main.js
@@ -0,0 +1,28 @@
+class Slider{
+    constructor(selector){
+        this.move = this.move.bind(this);
+        this.slider = document.querySelector(selector);
+        this.interval = null;
+        this.contador = 0;
+        this.start();
+        
+    }
+    start() {
+        this.interval = window.setInterval(this.move,5000);
+    }
+    move() {
+        let itemsCount = document.querySelectorAll(".container > *").length;
+        console.log(itemsCount);
+        this.contador ++;
+        if(this.contador > itemsCount -1) this.contador = 0;
+        this.moveTo(this.contador);
+    }
+    moveTo(index){
+        let left = index * 100;
+        this.slider.querySelector(".container").style.left = "-"+left+"%";
+    }
+}
+
+(function(){
+    new Slider(".slider");
+})();
